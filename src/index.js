c38091afddd210cac82b11edb36d594717c5ca6c
elty-annie <eltinangwarai@gmail.com>
2024-01-17 16:40:13 +0100

    added the ai api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generate");
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.getElementById("user-instructions");
+  let apiKey = "45d862b440fa3dtc36ab8b0be0f20o60";
+  let context =
+    "You are a romantic German poett and love to write short poems.You should generate a 4 line poem in basic HTML and separate each line with a <br/>.Make sure to follow the instuctions.Do not include a title.";
+  let prompt = `User instructions:Generate a German poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log(` Prompt: ${prompt} `);
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElemnt = document.querySelector("#poem-form");
