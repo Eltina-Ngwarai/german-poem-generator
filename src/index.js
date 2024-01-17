ca71c265f23545ef9732cc0b5fac8183f02c5fa8
elty-annie <eltinangwarai@gmail.com>
2024-01-17 14:53:22 +0100

    added basic form handling and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElemnt = document.querySelector("#poem-form");
+poemFormElemnt.addEventListener("submit", generatePoem);
